5db0427005e3f80ce3b2da18b2baf42914925eec
rashmikr222 <rashmi.kr@dollarbirdinc.com>
2022-07-05 16:22:20 +0530

    service worker file updated

--- push.js
+++ push.js
@@ -1,6 +1,6 @@
-const push = require('web-push');
+const webpush = require('web-push');
 
-// const vapidKeys = push.generateVAPIDKeys();
+// const vapidKeys = webpush.generateVAPIDKeys();
 // console.log("keys -",vapidKeys);
 const vapidKeys = {
     publicKey: 'BN5_gMwOfJTL8B4LmPTUlYAs7gCwJPkgN6748y1GPOpzF1pPpHePdBmyhgKTFCPugxWHkeJwwQ1kpFz8CHyACmg',
@@ -14,11 +14,10 @@ webpush.setVapidDetails(
   );
 
   const sub = {
-    endpoint: '.....',
-    keys: {
-      auth: '.....',
-      p256dh: '.....'
-    }
+    endpoint:"https://fcm.googleapis.com/fcm/send/dPC7MifpgMw:APA91bEolbnc9PNdYk3MN7G_FAN5hdPoZJsUBUQlosInWfGiYNN2K36s_fUoZ89LJN8eoka34bkc_DI-6-tlb0-oIcMdZm2K5LYMjG2pOqbCuQNhoF5gvzelyi90sAitvCobDpYz7yqF",
+    expirationTime:null,
+    keys:{p256dh:"BFazx6PLy7Siw7lsoEu7Lsd2pUY6VLjaFi6XHnD7UruhFV46Mp3D949qwuHCpl_23NywcrvhzpdwULB3FKlOoEo",
+    auth:"C2FAm5DSO7y-VjKDcgb_ew"}
   };
 
   webpush.sendNotification(sub, 'Your Push Payload Text');
--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('push',()=>{
-    self.registration.sendNotification('test message',{});
+self.addEventListener('push',(e)=>{
+    var options = {
+        body:'This notification was generated from a push',
+        data : {
+            dateOfArrivals : Date.now(),
+        },
+        actions:[
+            {
+                action:'explore',
+                title:'explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action:'close',
+                title:'close',
+                icon: 'images/xmark.png'
+            }
+        ]
+    };
+    e.waitUntil(
+        self.registration.sendNotification('test message',options)
+    );
+    
 });
